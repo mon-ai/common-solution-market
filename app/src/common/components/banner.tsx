83ae68322c7d4b7379940d7f5ce3db2648da48a7
疒奀 <yz@mmon.co>
2022-04-14 16:08:11 -0700

    add noreferrer to link

--- app/src/common/components/banner.tsx
+++ app/src/common/components/banner.tsx
@@ -2,7 +2,7 @@ export default function Banner() {
   return (
     <div className="flex flex-row flex-nowrap w-screen h-20">
       <div className="bg-white w-1/2 flex flex-row flex-wrap justify-between px-4">
-        <a href="https://mmonai.co/" target="_blank">
+        <a href="https://mmonai.co/" target="_blank" rel="noreferrer">
           <h1 className="font-title text-3xl">
             <span className="text-ant">.co</span>
             <br />
